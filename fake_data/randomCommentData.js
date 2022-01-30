f25871fc25b5d2367be66c3b4bddb579b77b2ba4
MartM <martmartez4@gmail.com>
2022-01-29 19:50:41 -0700

    Created a function to create fake json data to act as comments, the SQL Query will handle sorting comments by date, and should return data in the format  similar to the  fake data created

---
+++ fake_data/randomCommentData.js
@@ -0,0 +1,94 @@
+
+const faker = require("faker");
+
+const UUIDv4 = require('uuid') ;
+
+const randName = faker.commerce.productDescription();
+const randName2 = faker.company.catchPhrase();
+
+const fs = require('fs');
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+
+// console.log(randName);
+// console.log(randName2);
+
+
+data = `
+"sorted_comments":{`
+    
+//     "1":{
+        
+//         "34234234234":{
+            
+//             "date_made":"12/23/2021",
+//             "text_content": "${randName + ' ' +randName2}",
+//             "like": "10",
+//             "replies": []
+//         }
+//     }   
+// }
+// `
+
+let tempData = ``;
+
+let numberUsers = 6;
+
+for(let i = 1; i <= numberUsers; i++ ){
+
+    // console.log("ping")
+
+    tempData += `
+                    "${i}": {
+    `
+    
+    let randNum = getRndInteger(0, 15)
+
+    let counter = 0;
+    // console.log(randNum)
+
+    while(counter < randNum){
+
+
+        const randName = faker.commerce.productDescription();
+        const randName2 = faker.company.catchPhrase();
+
+        const randomDate = faker.date.past();
+
+        tempData = tempData + `
+                                "${UUIDv4.v4()}": 
+                                {
+                                    "date_made":"${randomDate}",
+
+                                    "text_content": "${randName + ' ' +randName2}",
+                                    
+                                    "like": "10",
+                                    "replies": []
+                                }${counter < randNum-1 ? ",":""}       
+        `
+
+        counter++;
+
+        // randNum = getRndInteger(0, 10)
+
+    }
+
+    
+    // console.log(tempData);
+    tempData += `}${i < numberUsers? ",":""}`;
+}
+
+data = data + tempData + '}';
+
+// console.log(data)
+
+// console.log(data);
+
+fs.appendFile('testfake.json', data, function (err) {
+    if (err) throw err;
+    console.log('Updated!');
+    });
+
